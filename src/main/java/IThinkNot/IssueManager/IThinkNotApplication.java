bceba76486c9b62a4f58785141f9234c4bc81b26
dongyoung kim <kibox327@gmail.com>
2017-01-31 13:55:11 +0900

    init commit

---
+++ src/main/java/IThinkNot/IssueManager/IThinkNotApplication.java
@@ -0,0 +1,12 @@
+package IThinkNot.IssueManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IThinkNotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IThinkNotApplication.class, args);
+	}
+}
