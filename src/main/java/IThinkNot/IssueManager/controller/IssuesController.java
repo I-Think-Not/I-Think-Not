0fbe6c7b2e0b7bf5c763cb5b5b0c94a5317f7817
dongyoung kim <kibox327@gmail.com>
2017-01-31 15:20:56 +0900

    UserController MilestonesController IssuesController add

---
+++ src/main/java/IThinkNot/IssueManager/controller/IssuesController.java
@@ -0,0 +1,8 @@
+package IThinkNot.IssueManager.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class IssuesController {
+
+}
---
+++ src/main/java/IThinkNot/IssueManager/controller/MilestonesController.java
@@ -0,0 +1,10 @@
+package IThinkNot.IssueManager.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/milestone")
+public class MilestonesController {
+
+}
