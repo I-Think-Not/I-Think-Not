a958196b341be5196995f254e4c19115e7c99993
dongyoung kim <kibox327@gmail.com>
2017-02-01 16:24:00 +0900

    convention modify

--- src/main/java/IThinkNot/IssueManager/domain/Issue.java
+++ src/main/java/IThinkNot/IssueManager/domain/Issue.java
@@ -7,6 +7,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
@@ -15,19 +16,16 @@ public class Issue {
 	@Id
 	@GeneratedValue
 	private long id;
-	
 	@Column(length = 100, nullable = false)
 	private String subject;
-	@Column(length = 2000)
+	@Lob
 	private String contents;
 	@Column(nullable = false)
-	private Date writeDate;
-	
+	private Date creationDate;
 	@ManyToOne
 	private User writer;
 	@ManyToOne
 	private Milestone milestone;
-
 	@OneToMany
 	private List<Label> labels;
 	@OneToMany
@@ -35,121 +33,83 @@ public class Issue {
 	@OneToMany
 	private List<Comment> comments;
 	
-	public Issue() {
-		
-	}
+	public Issue() {}
 	
 	public long getId() {
 		return id;
 	}
 
-
-
 	public void setId(long id) {
 		this.id = id;
 	}
-
-
 
 	public String getSubject() {
 		return subject;
 	}
 
-
-
 	public void setSubject(String subject) {
 		this.subject = subject;
 	}
-
-
 
 	public String getContents() {
 		return contents;
 	}
 
-
-
 	public void setContents(String contents) {
 		this.contents = contents;
 	}
 
-
-
-	public Date getWriteDate() {
-		return writeDate;
+	public Date getCreationDate() {
+		return creationDate;
 	}
 
-
-
-	public void setWriteDate(Date writeDate) {
-		this.writeDate = writeDate;
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
 	}
-
-
 
 	public User getWriter() {
 		return writer;
 	}
 
-
-
 	public void setWriter(User writer) {
 		this.writer = writer;
 	}
-
-
 
 	public Milestone getMilestone() {
 		return milestone;
 	}
 
-
-
 	public void setMilestone(Milestone milestone) {
 		this.milestone = milestone;
 	}
-
-
 
 	public List<Label> getLabels() {
 		return labels;
 	}
 
-
-
 	public void setLabels(List<Label> labels) {
 		this.labels = labels;
 	}
-
-
 
 	public List<User> getAssines() {
 		return assines;
 	}
 
-
-
 	public void setAssines(List<User> assines) {
 		this.assines = assines;
 	}
-
-
 
 	public List<Comment> getComments() {
 		return comments;
 	}
 
-
-
 	public void setComments(List<Comment> comments) {
 		this.comments = comments;
 	}
 
-
-
 	@Override
 	public String toString() {
-		return "Issue [id=" + id + ", subject=" + subject + ", contents=" + contents + ", writeDate=" + writeDate
+		return "Issue [id=" + id + ", subject=" + subject + ", contents=" + contents + ", creationDate=" + creationDate
 				+ ", writer=" + writer + ", milestone=" + milestone + ", labels=" + labels + ", assines=" + assines
 				+ ", comments=" + comments + "]";
 	}
