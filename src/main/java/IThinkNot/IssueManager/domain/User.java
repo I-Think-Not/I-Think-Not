383a06c2e441e23c0ea944d195782ac73064b92f
Woo Young <clearpal7@gmail.com>
2017-02-01 14:26:11 +0900

    UserDomain Updated

--- src/main/java/IThinkNot/IssueManager/domain/User.java
+++ src/main/java/IThinkNot/IssueManager/domain/User.java
@@ -1,5 +1,70 @@
 package IThinkNot.IssueManager.domain;
 
-public class User {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class User {
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	@Column(nullable = false, length = 20, unique = true)
+	private String name;
+	
+	@Column(nullable = false, length = 20, unique = true)
+	private String userId;
+	
+	@JsonIgnore
+	private String password;
+	
+	private String profile;
+
+	public User(){}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getProfile() {
+		return profile;
+	}
+
+	public void setProfile(String profile) {
+		this.profile = profile;
+	}
+	
+	
 }
