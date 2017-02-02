81617fae06a99dc2f2df8cd6e4ddac4c81731227
Woo Young <clearpal7@gmail.com>
2017-02-02 13:51:34 +0900

    Add UserRespository, Modify User, Add function UserController

--- src/main/java/itn/issuemanager/controller/UserController.java
+++ src/main/java/itn/issuemanager/controller/UserController.java
@@ -4,7 +4,9 @@ import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,18 +15,19 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import itn.issuemanager.domain.User;
+import itn.issuemanager.domain.UserRepository;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
 	
 	private static final Logger log = LoggerFactory.getLogger(UserController.class);
+	private final String USER_SESSION_KEY = "sessionedUser";
 	
-	//초기 페이지
-	@GetMapping("/")
-	public String index(){
-		return "/index";
-	}
+	@Autowired
+	private UserRepository userRepository;
+	
+	
 	//회원가입 페이지
 	@GetMapping("/new")
 	public String form(){
@@ -33,25 +36,45 @@ public class UserController {
 	//회원가입 기능
 	@PostMapping("/join")
 	public String create(User user){
-		//log.debug(user);
-		//userRepository.save(user);
-		return "redirect:/users";
+		log.debug("User :" + user.toString());
+		userRepository.save(user);
+		return "redirect:/users/login";
 	}
 	//회원 로그인
 	@GetMapping("/login")
-	public String login(){
+	public String login(String userId, String password, HttpSession session){
 		return "/user/login";
 	}
 	//회원수정 페이지
 	@GetMapping("/{id}/edit")
-	public String edit(){
+	public String edit(@PathVariable long id, Model model, HttpSession session){
+		if(isLoginUser(session)){
+			return "redirect:/users/login";
+		}
 		
+		User sessionedUser = getUserFromSession(session);
+		if(!sessionedUser.matchedId(id)){
+			throw new IllegalStateException("You can't update the anther user");
+		}
+		User user = userRepository.findOne(id);
+		model.addAttribute("user",user);
 		return "/user/updateform";
 	}
 	//회원수정 메소드
 	@PutMapping("/{id}")
-	public String update(@PathVariable long id, User user, HttpSession session){
+	public String update(@PathVariable long id, User updatedUser, HttpSession session){
+		if(isLoginUser(session)){
+			return "redirect:/users/login";
+		}
 		
+		User sessionedUser = getUserFromSession(session);
+		if (!sessionedUser.matchedId(id)) {
+			throw new IllegalStateException("You can't update the anther user");
+		}
+		
+		User user = userRepository.findOne(id);
+		user.update(updatedUser);
+		userRepository.save(user);
 		return "redirect:/users";
 	}
 	//회원삭제 메소드
@@ -60,6 +83,23 @@ public class UserController {
 		
 		return "redirect:/users";
 	}
+	//로그인 여부 확인
+	public boolean isLoginUser(HttpSession session){
+		Object sessionedUser = session.getAttribute(USER_SESSION_KEY);
+		if(sessionedUser == null){
+			return false;
+		}
+		return true;
+	}
+	//세션으로부터 User 가져오기
+	public User getUserFromSession(HttpSession session){
+		if(!isLoginUser(session)){
+			return null;
+		}
+		return (User)session.getAttribute(USER_SESSION_KEY);
+	}
+	
+	
 	
 	
 }
---
+++ src/main/java/itn/issuemanager/domain/UserRepository.java
@@ -0,0 +1,7 @@
+package itn.issuemanager.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long>{
+	User findByUserId(String userId);
+}
