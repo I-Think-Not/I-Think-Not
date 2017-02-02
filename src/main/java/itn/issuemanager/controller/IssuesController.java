df46a50f71d94d0efbabc2e71fb647ef922e0a9d
Hyoungjin Bae <impazziamoci@gmail.com>
2017-02-02 14:17:17 +0900

    issue list and issue show
    이슈 리스트 보기 와 이슈 상세보기

--- src/main/java/itn/issuemanager/controller/IssuesController.java
+++ src/main/java/itn/issuemanager/controller/IssuesController.java
@@ -1,9 +1,13 @@
 package itn.issuemanager.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +19,16 @@ import itn.issuemanager.domain.IssuesRepository;
 @RequestMapping("/issue")
 public class IssuesController {
 	
+	private static final Logger log = LoggerFactory.getLogger(IssuesController.class);
+
 	@Autowired
 	private IssuesRepository issuesRepository;
 	
+	@GetMapping("/")	//이슈 리스트
+	public String list(Model model) {
+		model.addAttribute("issues", issuesRepository.findAll());
+		return "index";
+	}
 	
 	@GetMapping("/new")	//생성 폼
 	public String form() {
@@ -32,9 +43,10 @@ public class IssuesController {
 	}
 	
 	@GetMapping("/{id}")	//상세보기
-	public String show() {
-		
-		return "index";
+	public String show(@PathVariable Long id, Model model) {
+		Issue dbIssue = issuesRepository.findOne(id);
+		model.addAttribute("issue", dbIssue);
+		return "issue/show";
 	}
 	
 	@GetMapping("/{id}/edit")	//수정 폼
