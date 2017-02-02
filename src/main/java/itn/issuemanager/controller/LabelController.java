bbd0dc21d9c26a3f4fe9d9e6e760523e04400d0b
dongyoung kim <kibox327@gmail.com>
2017-02-02 13:35:34 +0900

    label list controller apply

--- src/main/java/itn/issuemanager/controller/LabelController.java
+++ src/main/java/itn/issuemanager/controller/LabelController.java
@@ -1,15 +1,19 @@
 package itn.issuemanager.controller;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import itn.issuemanager.domain.Label;
 import itn.issuemanager.domain.LabelRepository;
 
 @Controller
@@ -23,13 +27,26 @@ public class LabelController {
 	@GetMapping("/new")	//생성 폼
 	public String form() {
 		
-		return "index";
+		return "label/form";
 	}
 	
-	@PostMapping("/")	//생성
-	public String create() {
+	@GetMapping("/")	//리스트 보기
+	public String list(Model model) {
+		log.debug("list access");
 		
-		return "redirect:/";
+		
+		List<Label> labelList = (List<Label>) labelRepository.findAll();
+		
+		model.addAttribute("labelList",labelList);
+		
+		return "label/list";
+	}
+	
+	
+	@PostMapping("/")	//생성
+	public String create(Label inputLabel) {
+		labelRepository.save(inputLabel);
+		return "redirect:/label/";
 	}
 	
 	@GetMapping("/{id}")	//상세보기
@@ -41,19 +58,19 @@ public class LabelController {
 	@GetMapping("/{id}/edit")	//수정 폼
 	public String edit() {
 		
-		return "index";
+		return "label/form";
 	}
 	
 	@PutMapping("/{id}")	//수정하기
 	public String update() {
 		
-		return "redirect:/";
+		return "redirect:/label/";
 	}
 	
 	@DeleteMapping("/{id}")	//삭제
 	public String delete() {
 		
-		return "redirect:/";
+		return "redirect:/label/";
 	}
 
 }
