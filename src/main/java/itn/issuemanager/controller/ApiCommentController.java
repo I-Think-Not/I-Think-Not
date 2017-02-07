f41063d86a1238702cf96cd9798bfc94d6d42544
Hyoungjin Bae <impazziamoci@gmail.com>
2017-02-07 17:49:18 +0900

    comment 수정 삭제 ajax로 전환

---
+++ src/main/java/itn/issuemanager/controller/ApiCommentController.java
@@ -0,0 +1,64 @@
+package itn.issuemanager.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import itn.issuemanager.domain.Comment;
+import itn.issuemanager.domain.User;
+import itn.issuemanager.repository.CommentRepository;
+import itn.issuemanager.repository.IssuesRepository;
+import itn.issuemanager.repository.LabelRepository;
+import itn.issuemanager.repository.MilestoneRepository;
+
+
+@RestController
+@RequestMapping("/api/issue/{issueId}/comment")
+public class ApiCommentController {
+	
+	private static final Logger log = LoggerFactory.getLogger(LabelController.class);
+	private final String USER_SESSION_KEY = "sessionedUser";
+
+	@Autowired
+	private IssuesRepository issuesRepository;
+	@Autowired
+	private CommentRepository commentRepository;
+	
+
+	@PostMapping("/create")
+	@ResponseBody
+	public Comment create(@PathVariable long issueId, Comment comment, HttpSession session) {
+		Comment newComment = new Comment(comment, (User) session.getAttribute(USER_SESSION_KEY), issuesRepository.findOne(issueId));
+		return commentRepository.save(newComment);
+	}
+
+	@GetMapping("/{id}/edit")
+	public String edit() {
+		return "issue/show";
+	}
+
+	@PutMapping("/{id}")
+	public String update() {
+		return "";
+	}
+
+	@DeleteMapping("/{id}")
+	public Comment delete(@PathVariable long issueId, @PathVariable long id) {
+		Comment comment = commentRepository.findOne(id);
+		commentRepository.delete(comment);
+		return comment;
+	}
+}
