95348cd90921ca870a9e2fa5ba348c4f60adf80a
javajigi <javajigi@gmail.com>
2017-02-08 11:16:56 +0900

    second review

--- src/main/java/net/slipp/utils/DateTimeUtils.java
+++ src/main/java/net/slipp/utils/DateTimeUtils.java
@@ -3,6 +3,7 @@ package net.slipp.utils;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+// TODO 중복 클래스. 제거 필요함.
 public class DateTimeUtils {
 	public static String format(Date date, String format) {
 		if (date == null) {
