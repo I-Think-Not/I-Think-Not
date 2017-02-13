b3d8e8cc3616057219dce65a9239baad61a45c6b
Jeongyeon Lee <dg6676@gmail.com>
2017-02-13 17:54:06 +0900

    modif issuereRespotiroy find query

--- src/main/java/itn/issuemanager/repository/IssuesRepository.java
+++ src/main/java/itn/issuemanager/repository/IssuesRepository.java
@@ -1,8 +1,14 @@
 package itn.issuemanager.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import itn.issuemanager.domain.Issue;
+import itn.issuemanager.domain.IssueState;
+import itn.issuemanager.domain.Milestone;
 
 public interface IssuesRepository extends JpaRepository<Issue, Long>{
+	List<Issue> findByMilestoneAndState(Milestone milestone, IssueState state);
+	List<Issue> findByState(IssueState state);
 }
--- src/main/resources/static/js/milestone.js
+++ src/main/resources/static/js/milestone.js
@@ -1,14 +1,29 @@
-var openIssueList = document.querySelector("#openIssueList");
-var closeIssueList = document.querySelector("#closeIssueList");
+/*var openIssueList = document.querySelector("#openIssueList");
+var closeIssueList = document.querySelector("#closeIssueList");*/
 
+var myLib=(function(){
+	var openIssueList = function(){
+		return $("#openIssueList");
+	};
+	var closeIssueList = function(){
+		return $("#closeIssueList");
+	};
+	
+	return {
+		openIssueList : openIssueList,
+		closeIssueList : closeIssueList
+	};
+})();
 
 function addClickEvent(){
-	openIssueList.addEventListener("click",function(){
-		var url="/milestone/api/"+{{id}}+"/openIssueList";
+	myLib.openIssueList().click(function(e){
+		var event = $(e.target);
+		var url="/milestone/api/"+event.data("issueId")+"/openIssues";
 		ajax(url);
 	});
-	closeIssueList.addEventListener("click",function(){
-		var url="/milestone/api/{{id}}/closeIssueList";
+	myLib.closeIssueList().click(function(e){
+		var event = $(e.target);
+		var url="/milestone/api/"+event.data("issueId")+"/closeIssues";
 		ajax(url);
 	});
 }
