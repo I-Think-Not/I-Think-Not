f1b641f2d3d0667f4f01e755ebfcd2a7d22a9468
dongyoung kim <kibox327@gmail.com>
2017-02-17 10:10:42 +0900

    error check....?

--- src/main/java/itn/issuemanager/controller/ControllerExceptionHandler.java
+++ src/main/java/itn/issuemanager/controller/ControllerExceptionHandler.java
@@ -1,22 +1,32 @@
 package itn.issuemanager.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+import itn.issuemanager.domain.ForbiddenTypeException;
 import itn.issuemanager.domain.ForbiddenTypeFileException;
 
 
 @Controller
 public class ControllerExceptionHandler implements ErrorController{
 
+	
+	private static final Logger log = LoggerFactory.getLogger(ControllerExceptionHandler.class);
+
 	private static final String PATH = "/error";
 	
+	@ResponseStatus(value=HttpStatus.INTERNAL_SERVER_ERROR)
 	@RequestMapping(value = PATH)
 	@ExceptionHandler(Exception.class)
-	public String ForbiddenTypeError() {
+	public String DefaultError() {
+		log.debug("exception");
 		return "exception/serverError";
 	}
 	
